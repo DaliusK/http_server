7ed01c4a1d1a4bacef44dbc95a1d41fa88a2ed75
Paulius Šukys <paul.sukys@gmail.com>
2014-09-20 11:56:27 +0300

    Multiple additions for externalizing setting of website root:  - operating system function (not stable) to deduce what OS is being used  - return executable's path  - substring function (outsourced code) for parsing path  - helper function for substring

--- src/helper.c
+++ src/helper.c
@@ -1,9 +1,138 @@
 #include "helper.h"
 #include <ctype.h>
+#include <stdio.h>
+#include <unistd.h>
 
 void to_lowercase(char *string)
 {
     int i;
     for (i = 0; string[i]; i++)
         string[i] = tolower(string[i]);
+}
+
+OS get_os()
+{
+    //help from http://nadeausoftware.com/articles/2012/01/c_c_tip_how_use_compiler_predefined_macros_detect_operating_system#Howtodetecttheoperatingsystemtype
+    #if defined(__unix__) || (defined(__APPLE__) && defined(__MACH__))
+    return BSD;
+    #elif defined(__linux__)
+    return LINUX;
+    #elif defined(__sun) && defined(__SVR4)
+    return SOLARIS;
+    #elif defined(__WIN64) || defined(__WIN32)
+    return WINDOWS;
+    #endif
+    return OTHER_UNIX;
+}
+
+char* get_exec_path(char *buf, int bufsize)
+{
+    //guided by http://stackoverflow.com/a/933996/552214
+    printf("Get executable's path\n");
+    
+    readlink("/proc/self/exe", buf, bufsize);
+    printf("\tPath: %s\n", buf);
+    return buf;
+}
+
+void substr(char *buffer, size_t buflen, char const *source, int len)
+{
+    size_t srclen = strlen(source);
+    size_t nbytes = 0;
+    size_t offset = 0;
+    size_t sublen;
+
+    if (buflen == 0)    /* Can't write anything anywhere */
+        return;
+
+    if (len > 0)
+    {
+        sublen = len;
+        /* if substr index is bigger than source last index */
+        nbytes = (sublen > srclen) ? srclen : sublen;
+        offset = 0;
+    }
+    else if (len < 0)
+    {
+        sublen = -len;
+        nbytes = (sublen > srclen) ? srclen : sublen;
+        offset = srclen - nbytes;
+    }
+
+    if (nbytes >= buflen)
+        nbytes = 0;
+
+    if (nbytes > 0)
+        memmove(buffer, source + offset, nbytes);
+    buffer[nbytes] = '\0';
+}
+
+int indexOf_shift (char* base, char* str, int startIndex) 
+{
+    int result;
+    int baselen = strlen(base);
+    // str should not longer than base
+    if (strlen(str) > baselen || startIndex > baselen)
+        result = -1;
+    else
+    {
+        if (startIndex < 0 )
+            startIndex = 0;
+
+        char* pos = strstr(base+startIndex, str);
+        if (pos == NULL)
+            result = -1;
+        else
+            result = pos - base;
+        
+    }
+    return result;
+}
+
+int lastIndexOf(char* base, char* str)
+{
+    int result;
+    // str should not longer than base
+    if (strlen(str) > strlen(base))
+        result = -1;
+    else
+    {
+        int start = 0;
+        int endinit = strlen(base) - strlen(str);
+        int end = endinit;
+        int endtmp = endinit;
+        while(start != end) {
+            start = indexOf_shift(base, str, start);
+            end = indexOf_shift(base, str, end);
+
+            // not found from start
+            if (start == -1)
+                end = -1; // then break;
+            else if (end == -1) 
+            {
+                // found from start
+                // but not found from end
+                // move end to middle
+                if (endtmp == (start+1))
+                    end = start; // then break;
+                else
+                {
+                    end = endtmp - (endtmp - start) / 2;
+                    if (end <= start)
+                        end = start+1;
+                    endtmp = end;
+                }
+            }
+            else
+            {
+                // found from both start and end
+                // move start to end and
+                // move end to base - strlen(str)
+                start = end;
+                end = endinit;
+            }
+        }
+        result = start;
+    }
+    return result;
 }
--- src/helper.h
+++ src/helper.h
@@ -1,8 +1,65 @@
 #ifndef HELPER_H_
 #define HELPER_H_
 
+#include <string.h>
 /** Turns a string into lowercase
  */
 void to_lowercase(char *string);
+
+/** Needed for defining where there server is being run
+ */
+typedef enum
+{
+    WINDOWS,
+    LINUX,
+    BSD,
+    SOLARIS,
+    OTHER_UNIX
+} OS;
+
+/** Returns the operating system enum
+ */
+OS get_os();
+
+/** Returns executable's directory
+ */
+char * get_exec_path();
+
+/*
+** Given substr(buffer, sizeof(buffer), "string", len), then the output
+** in buffer for different values of len is:
+** For positive values of len:
+** 0    ""
+** 1    "s"
+** 2    "st"
+** ...
+** 6    "string"
+** 7    "string"
+** ...
+** For negative values of len:
+** -1   "g"
+** -2   "ng"
+** ...
+** -6   "string"
+** -7   "string"
+** ...
+** Subject to buffer being long enough.
+** If buffer is too short, the empty string is set (unless buflen is 0,
+** in which case, everything is left untouched).
+*/
+void substr(char *buffer, size_t buflen, char const *source, int len);
+
+/* int indexOf_shift (char* base, char* str, int startIndex)
+ *            Custom function for getting the first index of str in base
+ *            after the given startIndex
+ */
+int indexOf_shift(char* base, char* str, int startIndex);
+
+/** use two index to search in two part to prevent the worst case
+ * (assume search 'aaa' in 'aaaaaaaa', you cannot skip three char each time)
+ * code attributed to http://ben-bai.blogspot.com/2013/03/c-string-startswith-endswith-indexof.html
+ */
+int lastIndexOf(char* base, char* str);
+
 
 #endif
