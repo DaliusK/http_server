7bfa2d31a5ba2248757c249aa2d8b7676c0f4fb8
Paulius Šukys <paul.sukys@gmail.com>
2014-09-24 16:19:51 +0300

    Stripped out main method into separate file

--- src/server.c
+++ src/server.c
@@ -191,7 +191,6 @@ int process_request(FILE *f, char *root)
     relative_path = strtok(NULL, " ");
     char *path = calloc(strlen(root) + strlen(relative_path) + 1, sizeof(char));
     
-    
     memmove(path, root, strlen(root));
     path[strlen(root) + 1] = '\0';
     strcat(path, relative_path);
@@ -271,50 +270,4 @@ int loop(int sock, char* root)
     process_request(f, root);
     fclose(f);
     return 0;
-}
-
-int main(int argc, char *argv[])
-{
-    if (argc < 2)
-    {
-        printf("Usage: %s PORT\n", argv[0]);
-        printf("\tPORT - a number which represents the port (1-65535)\n");
-        printf("\t\tusually 80 is for web\n");
-        return 1;
-    }
-
-    /* Separate the process and make as a daemon */
-    if (fork() != 0)
-        return 1;//failed to fork
-
-    //ignore some shutdown signals: http://stackoverflow.com/questions/18551485/how-to-make-the-process-ignore-some-signallike-sighup-sigabrt-sigabort-sigint-e
-    signal(SIGHUP, SIG_IGN);//ignore terminal exits
-    signal(SIGCLD, SIG_IGN);//ignore child exits
-    int port = atoi(argv[1]);
-    int sock;
-    struct sockaddr_in sin;
-    sock = socket(AF_INET, SOCK_STREAM, 0);
-
-    sin.sin_family = AF_INET;
-    sin.sin_addr.s_addr = INADDR_ANY;
-    sin.sin_port = htons(port);
-
-    if (bind(sock, (struct sockaddr * ) &sin, sizeof(sin)) != 0)
-    {
-        printf("Failed to bind to socket\n");
-        return 1;
-    }
-
-
-    listen(sock, 5);
-    printf("HTTP server listening on port %d\n", port);
-    char *root = get_root();
-    int loop_result = loop(sock, root);
-    while (loop_result != 1)
-    {
-        loop_result = loop(sock, root);
-    }
-    free(root);
-    close(sock);
-    return loop_result;
 }
