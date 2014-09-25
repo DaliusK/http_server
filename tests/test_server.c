f53c450f7c6449970d13f4993412f2ee9be2b9cc
Paulius Šukys <paul.sukys@gmail.com>
2014-09-25 11:07:23 +0300

    Change regressive read_from_file to simply is_in_file, which read the contents and check via strstr if specific substring is in the file removed slashes for roots to make relative paths Added test case with testing invalid requests

--- tests/test_server.c
+++ tests/test_server.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/stat.h>
 #include "minunit.h"
 #include "../src/server.h"
 
@@ -13,7 +14,7 @@ void write_to_file(const char * filename, char * text)
     fclose(f);
 }
 
-void read_from_file(const char * filename, char * string)
+int is_in_file(const char * filename, char * substr)
 {
     FILE *f = fopen(filename, "rb");
     //find out the size
@@ -21,10 +22,21 @@ void read_from_file(const char * filename, char * string)
     int file_size = ftell(f);
     //get back and start reading
     fseek(f, 0L, SEEK_SET);
-    string = malloc(file_size * sizeof(char));
-    fread(string, sizeof(char), file_size, f);
+    char *str = malloc(file_size * sizeof(char));
+    fread(str, sizeof(char), file_size, f);
     fclose(f);
+    if (strstr(str, substr) == NULL)
+    {
+        free (str);
+        return 0;
+    }
+    else
+    {
+        free (str);
+        return 1;
+    }
 }
+
 /*
  * Feed a request for a non existing file (via non existing directory)
  * Expected to choke... Nah, just return 404
@@ -32,7 +44,7 @@ void read_from_file(const char * filename, char * string)
 static char * test_process_request_not_found()
 {
     const char * FILENAME = "test_server_not_found";
-    char * ROOT = "/non_existing_root";
+    char * ROOT = "non_existing_root";
     //this would be a blocker, thus needs to get reported
     write_to_file(FILENAME, "GET / HTTP/1.1");
 
@@ -40,10 +52,8 @@ static char * test_process_request_not_found()
     mu_assert("Error, f == NULL", f != NULL);
     process_request(f, ROOT);
     fclose(f);
-    char * file_contents;
-    read_from_file(FILENAME, file_contents);
-
-    mu_assert("Error, returned file does not contain 404 error code", strstr(file_contents, "404") == NULL);
+    
+    mu_assert("Error, returned file does not contain 404 error code", is_in_file(FILENAME, "404") == 1);
     remove(FILENAME);
     return 0;
 }
@@ -55,7 +65,17 @@ static char * test_process_request_not_found()
 static char * test_process_request_bad_method()
 {
     const char * FILENAME = "test_server_bad_method";
-    char * ROOT = "/not_important_dir";
+
+    char * ROOT = "not_important_dir";
+
+    int root_created = 0;
+    struct stat *st = {0};
+
+    if (stat(ROOT, st) == -1)
+    {
+        mkdir(ROOT, 0700);
+        root_created = 1;
+    }
 
     write_to_file(FILENAME, "BAD_METHOD / HTTP/1.1");
     FILE *f = fopen(FILENAME, "a+");
@@ -64,10 +84,72 @@ static char * test_process_request_bad_method()
 
     process_request(f, ROOT);
     fclose(f);
-    char * file_contents;
-    read_from_file(FILENAME, file_contents);
 
-    mu_assert("Error, returned file does not contain 501 error code", strstr(file_contents, "501") == NULL);
+    mu_assert("Error, returned file does not contain 501 error code", is_in_file(FILENAME, "501") == 1);
+
+    //sanitize after test
+    if (root_created == 1)
+        rmdir(ROOT);
+
+    remove(FILENAME);
+    return 0;
+}
+
+/* 
+ * Tests how request is parsed
+ * 1. Empty
+ * 2. Only method
+ * 3. Method and root
+ */
+static char * test_process_request_invalid_request()
+{
+    const char * FILENAME = "test_server_invalid_request";
+    char * ROOT = "invalid_request";
+
+    int root_created = 0;
+    struct stat *st = {0};
+
+    if (stat(ROOT, st) == -1)
+    {
+        mkdir(ROOT, 0700);
+        root_created = 1;
+    }
+
+    //simply empty
+    write_to_file(FILENAME, "");
+
+    FILE *f = fopen(FILENAME, "a+");
+    mu_assert("Error, f == NULL", f != NULL);
+    int returncode = process_request(f, ROOT);
+    fclose(f);
+    remove(FILENAME);
+    mu_assert("Error, process_request should've returned -1", returncode == -1);
+
+
+    //only method
+    write_to_file(FILENAME, "GET");
+
+    f = fopen(FILENAME, "a+");
+    mu_assert("Error, f == NULL", f != NULL);
+    returncode = process_request(f, ROOT);
+    fclose(f);
+    remove(FILENAME);
+    mu_assert("Error, process_request should've returned -1", returncode == -1);
+
+    //method and root
+    write_to_file(FILENAME, "GET /");
+
+    f = fopen(FILENAME, "a+");
+    mu_assert("Error, f == NULL", f != NULL);
+    returncode = process_request(f, ROOT);
+    fclose(f);
+    mu_assert("Error, process_request should've returned -1", returncode == -1);
+
+
+    //sanitize after test
+    if (root_created == 1)
+        rmdir(ROOT);
+
     remove(FILENAME);
     return 0;
 }
@@ -76,6 +158,7 @@ static char * all_tests()
 {
     mu_run_test(test_process_request_not_found);
     mu_run_test(test_process_request_bad_method);
+    mu_run_test(test_process_request_invalid_request);
     return 0;
 }
 
