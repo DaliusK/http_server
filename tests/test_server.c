6577abcf2a9159bc3988dfe51c6dfa86b293a1f0
Paulius Šukys <paul.sukys@gmail.com>
2014-09-30 13:08:12 +0300

    Finished minimal test cases for process_request function

--- tests/test_server.c
+++ tests/test_server.c
@@ -43,6 +43,7 @@ int is_in_file(const char * filename, char * substr)
  */
 static char * test_process_request_not_found()
 {
+    printf("Test process request: File not found\n");
     const char * FILENAME = "test_server_not_found";
     char * ROOT = "non_existing_root";
     //this would be a blocker, thus needs to get reported
@@ -55,6 +56,7 @@ static char * test_process_request_not_found()
     
     mu_assert("Error, returned file does not contain 404 error code", is_in_file(FILENAME, "404") == 1);
     remove(FILENAME);
+    printf("\n");
     return 0;
 }
 
@@ -64,6 +66,7 @@ static char * test_process_request_not_found()
  */
 static char * test_process_request_bad_method()
 {
+    printf("Test process request: Bad method\n");
     const char * FILENAME = "test_server_bad_method";
 
     char * ROOT = "not_important_dir";
@@ -92,6 +95,7 @@ static char * test_process_request_bad_method()
         rmdir(ROOT);
 
     remove(FILENAME);
+    printf("\n");
     return 0;
 }
 
@@ -103,6 +107,7 @@ static char * test_process_request_bad_method()
  */
 static char * test_process_request_invalid_request()
 {
+    printf("Test process request: invalid request\n");
     const char * FILENAME = "test_server_invalid_request";
     char * ROOT = "invalid_request";
 
@@ -151,6 +156,152 @@ static char * test_process_request_invalid_request()
         rmdir(ROOT);
 
     remove(FILENAME);
+    printf("\n");
+    return 0;
+}
+
+static char * test_process_request_send_file()
+{
+    printf("Test process request: Send file\n");
+    const char * FILENAME = "test_server_send_file";
+    char * ROOT = "test_root_dir";
+    const char * TESTFILENAME = "test_root_dir/test_file";
+
+    int root_created = 0;
+    struct stat *st = {0};
+
+    if (stat(ROOT, st) == -1)
+    {
+        mkdir(ROOT, 0700);
+        root_created = 1;
+    }
+    write_to_file(TESTFILENAME, "send file test");
+    write_to_file(FILENAME, "GET /test_file HTTP/1.1");
+    FILE *f = fopen(FILENAME, "a+");
+
+    mu_assert("Error, f == NULL", f != NULL);
+
+    process_request(f, ROOT);
+    fclose(f);
+
+    mu_assert("Error, process_request should've returned file", is_in_file(FILENAME, "send file test") == 1);
+
+    //sanitize after test
+    remove(TESTFILENAME);
+
+    if (root_created == 1)
+        rmdir(ROOT);
+
+    remove(FILENAME);
+    printf("\n");
+    return 0;
+}
+
+static char * test_process_request_list_dir()
+{
+    printf("Test process request: List directory\n");
+    char * ROOT = "test_root_dir";
+    const char * FILENAME = "test_server_list_dir";
+    int root_created = 0;
+    struct stat *st = {0};
+
+    if (stat(ROOT, st) == -1)
+    {
+        mkdir(ROOT, 0700);
+        root_created = 1;
+    }
+
+    write_to_file(FILENAME, "GET / HTTP/1.1");
+    FILE *f = fopen(FILENAME, "a+");
+
+    mu_assert("Error, f == NULL", f != NULL);
+
+    process_request(f, ROOT);
+    fclose(f);
+
+    mu_assert("Error, should've printed directory listing", is_in_file(FILENAME, "Index of /") == 1);
+
+    //sanitize after test
+    if (root_created == 1)
+        rmdir(ROOT);
+
+    remove(FILENAME);
+    printf("\n");
+    return 0;
+}
+
+static char * test_process_request_no_dir_slash()
+{
+    printf("Test process request: List directory\n");
+    char * ROOT = "test_root_dir";
+    char * SUBDIR = "test_root_dir/subdir";
+    const char * FILENAME = "test_server_no_dir_slash";
+    int root_created = 0;
+    struct stat *st = {0};
+
+    if (stat(ROOT, st) == -1)
+    {
+        mkdir(ROOT, 0700);
+        mkdir(SUBDIR, 0700);
+        root_created = 1;
+    }
+
+    write_to_file(FILENAME, "GET /subdir HTTP/1.1");
+    FILE *f = fopen(FILENAME, "a+");
+
+    mu_assert("Error, f == NULL", f != NULL);
+
+    process_request(f, ROOT);
+    fclose(f);
+
+    mu_assert("Error, should've printed directory listing", is_in_file(FILENAME, "302") == 1);
+
+    //sanitize after test
+    if (root_created == 1)
+    {
+        rmdir(SUBDIR);
+        rmdir(ROOT);
+    }
+
+    remove(FILENAME);
+    printf("\n");
+    return 0;
+}
+
+static char * test_process_request_index_found()
+{
+    printf("Test process request: List directory\n");
+    char * ROOT = "test_root_dir";
+    const char * FILENAME = "test_server_index_found";
+    const char * INDEXFILE = "test_root_dir/index.html";
+    int root_created = 0;
+    struct stat *st = {0};
+
+    if (stat(ROOT, st) == -1)
+    {
+        mkdir(ROOT, 0700);
+        root_created = 1;
+    }
+
+    write_to_file(INDEXFILE, "test index file");
+    write_to_file(FILENAME, "GET / HTTP/1.1");
+    FILE *f = fopen(FILENAME, "a+");
+
+    mu_assert("Error, f == NULL", f != NULL);
+
+    process_request(f, ROOT);
+    fclose(f);
+
+    mu_assert("Error, should've printed directory listing", is_in_file(FILENAME, "test index file") == 1);
+
+    //sanitize after test
+    remove(INDEXFILE);
+
+    if (root_created == 1)
+        rmdir(ROOT);
+
+    remove(FILENAME);
+    printf("\n");
     return 0;
 }
 
@@ -159,6 +310,10 @@ static char * all_tests()
     mu_run_test(test_process_request_not_found);
     mu_run_test(test_process_request_bad_method);
     mu_run_test(test_process_request_invalid_request);
+    mu_run_test(test_process_request_send_file);
+    mu_run_test(test_process_request_list_dir);
+    mu_run_test(test_process_request_no_dir_slash);
+    mu_run_test(test_process_request_index_found);
     return 0;
 }
 
