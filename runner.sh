388d5818da26f54f87212629b304074b2b16c4ba
Paulius Šukys (RPi) <paul.sukys@gmail.com>
2015-03-25 07:33:30 +0000

    Move to 80th port

--- runner.sh
+++ runner.sh
@@ -1,12 +1,12 @@
-#!/bin/bash
+#!/bin/bash -x
 #lines: 1 for grep, 1 for the process
 cd /home/pi/src/http_server
 LOGFILE='http_server.log';
-lines=`ps x | grep -i "./bin/http_server 8080 >> logs/test.txt &" | wc -l`;
+lines=`ps x | grep -i "./bin/http_server 80 >> logs/server.log &" | wc -l`;
 
-if [[ $lines -ne 1 ]]
+if [[ $lines -ne 2 ]]
 then
     #means the process stopped, re-run it
     echo "\nHTTP server not found in process list, running again\n" >> $LOGFILE
-    ./bin/http_server 8080 >> logs/$LOGFILE &
+    ./bin/http_server 80 >> logs/$LOGFILE &
 fi
